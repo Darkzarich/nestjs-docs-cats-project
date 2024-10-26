41b8a5222a99f6be37d66481baefb00e606a898e
Darkzarich <darkzarichv2@gmail.com>
2024-10-26 16:52:24 +0300

    fix logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './logger/logger.middleware';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [CatsModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    // Middleware for specific routes
-    consumer.apply(LoggerMiddleware).forRoutes('cats');
+    consumer.apply(LoggerMiddleware).forRoutes(CatsController);
   }
 }
